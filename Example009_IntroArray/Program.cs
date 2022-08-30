e0c04b5b4b8e12bfee9232d42e0e183d65632713
Nikolay <n.astafyev@yandex.ru>
2022-08-31 00:58:05 +0800

    add Examples 9, 10, 11

--- Example009_IntroArray/Program.cs
+++ Example009_IntroArray/Program.cs
@@ -56,125 +56,20 @@
 
 
 
-// int Max(int arg1, int arg2, int arg3)
-// {
-//     int result = arg1;
-//     if(arg2 > result) result = arg2;
-//     if(arg3 > result) result = arg3;
-//     return result;
-// }
-
-// int[] array = {23, 455, 56, 434, 15, 89, 345, 76, 5};
-
-// int max = Max(
-//     Max(array[0], array[1], array[2]),
-//     Max(array[3], array[4], array[5]),
-//     Max(array[6], array[7], array[8])
-// );
-
-// Console.WriteLine(max);
-
-
-
-// метод для любого количества элементов в массиве 
-
-// int[] array = {1, 12, 31, 4, 18, 16, 17, 18};
-
-// int n = array.Length;
-
-// int find = 18;
-
-// int index = 0;
-
-// while (index < n)
-// {
-//     if(array[index] == find)
-//     {
-//         Console.WriteLine(index);
-//         break;
-//     }
-//     index++;
-// }
-
-
-// Новый массив, с определенным кол-вом элементов
-// метод void ничего не возвращает, соответственно в коде не используется оператор return
-
-// void FillArray(int[] collection)
-// {
-//     int Length = collection.Length;
-//     int index = 0;
-//     while (index < Length)
-//     {
-//         collection[index] = new Random().Next(1, 10);
-//         index++;
-//     }
-// }
-
-
-// void PrintArray(int[] col)
-// {
-//     int count = col.Length;
-//     int position = 0;
-//     while (position < count)
-//     {
-//         Console.WriteLine(col[position]);
-//         position++;
-//     }
-// }
-
-// int[] array = new int[10];
-
-// FillArray(array);
-// PrintArray(array);
-
-void FillArray(int[] collection)
+int Max(int arg1, int arg2, int arg3)
 {
-    int Length = collection.Length;
-    int index = 0;
-    while (index < Length)
-    {
-        collection[index] = new Random().Next(1, 10);
-        index++;
-    }
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
 }
 
+int[] array = {23, 455, 56, 434, 15, 89, 345, 76, 5};
 
-void PrintArray(int[] col)
-{
-    int count = col.Length;
-    int position = 0;
-    while (position < count)
-    {
-        Console.WriteLine(col[position]);
-        position++;
-    }
-}
+int max = Max(
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8])
+);
 
-int IndexOf(int[] collection, int find)
-{
-    int count = collection.Length;
-    int index = 0;
-    int position = 0;
-    while (index < count)
-    {
-        if(collection[index] == find)
-        {
-            position = index;
-            break;
-        }
-        index++;
-    }
-    return position;
-}
-
-int[] array = new int[10];
-
-FillArray(array);
-array[4]=4;
-array[6]=4;
-PrintArray(array);
-Console.WriteLine();
-
-int pos = IndexOf(array, 444);
-Console.WriteLine(pos);
+Console.WriteLine(max);
